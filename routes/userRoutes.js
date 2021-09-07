d564d62ebe0bf3f951f54e367452593353db6f7b
jerry <jerrynwosu007@gmail.com>
2021-09-07 15:17:54 +0100

    protect user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { loginUser, registerUser } = require('../controllers/userController');
+const {
+  loginUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} = require('../controllers/userController');
 
 const { registerValidation } = require('../helpers/validation');
+const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/login', loginUser);
 router.post('/register', registerValidation, registerUser);
+
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
