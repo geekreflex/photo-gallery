06c86f0abbbbe98f1368f486a7d350a3db2e1a94
jerry <jerrynwosu007@gmail.com>
2021-09-07 16:11:28 +0100

    photo schema

--- models/photoModel.js
+++ models/photoModel.js
@@ -1,1 +1,22 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const photoSchema = new Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId },
+    name: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    cloudId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Photo', photoSchema);
